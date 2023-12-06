1044e5b96cb1b3cdaed7063cf96e5ef1014ad85e
Matthijs Wesseling <matthijswesseling@gmail.com>
2023-12-06 14:19:26 +0100

    Smol cleanup

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -1,22 +1,19 @@
 use itertools::Itertools;
 
-fn find_sols(time: &usize, dist: &usize) -> usize {
+fn count_wins(time: &usize, dist: &usize) -> usize {
   let d = ((time.pow(2) - 4 * dist) as f64).sqrt();
-  ((*time as f64 + d) / 2. - 0.000001).floor() as usize - ((*time as f64 - d) / 2. + 0.000001).ceil()  as usize + 1
+  ((*time as f64 + d) / 2. - 0.01).floor() as usize - ((*time as f64 - d) / 2. + 0.01).ceil()  as usize + 1
 }
 
 #[aoc23::main(06)]
 fn main(input: &str) -> (usize, usize) {
   let (raw_time, raw_dist) = input.split_once("\n").unwrap();
+
   let time = raw_time.split_whitespace().skip(1).map(|n| n.parse::<usize>().unwrap()).collect_vec();
   let dist: Vec<usize> = raw_dist.split_whitespace().skip(1).map(|n| n.parse::<usize>().unwrap()).collect_vec();
-
-let p1 = time.iter().zip(dist.iter()).map(|(t, d)| find_sols(t, d)).product();
-
-let ctime = raw_time.split_whitespace().skip(1).join("").parse::<usize>().unwrap();
-let cdist = raw_dist.split_whitespace().skip(1).join("").parse::<usize>().unwrap();
-
-let p2 = find_sols(&ctime, &cdist);
-
-(p1, p2)
+  let ctime = raw_time.split_whitespace().skip(1).join("").parse::<usize>().unwrap();
+  let cdist = raw_dist.split_whitespace().skip(1).join("").parse::<usize>().unwrap();
+  
+  let p1 = time.iter().zip(dist.iter()).map(|(t, d)| count_wins(t, d)).product();
+  (p1, count_wins(&ctime, &cdist))
 }
