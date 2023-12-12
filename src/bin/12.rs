91c21df82ee02f7ef1ba3e294f9e172577f6a9f1
Matthijs Wesseling <matthijswesseling@gmail.com>
2023-12-12 19:11:45 +0100

    Add part 2 with optional cache

--- src/bin/12.rs
+++ src/bin/12.rs
@@ -1,43 +1,51 @@
+use std::collections::HashMap;
 use itertools::Itertools;
 
-fn parse_row(row: &str) -> (Vec<char>, Vec<char>) {
-  let (symbols, raw_counts) = row.split_once(' ').unwrap();
-  let counts = raw_counts.split(',').map(|num| num.parse::<usize>().unwrap()).collect_vec();
+fn count_perms(cache: &mut Option<HashMap<(usize, usize), usize>>, springs: &[u8], spaces: &[u8]) -> usize {
+  let key = (springs.len(), spaces.len());
+  match cache {
+    Some(ca) if ca.contains_key(&key) => return *ca.get(&key).unwrap(),
+    _ => ()
+  };
 
-  (
-    counts.iter().map(|n| "s".to_owned() + &("x".repeat(*n - 1))).collect_vec().join(" ").chars().collect_vec(),
-    symbols.chars().collect_vec()
-  )
-}
-
-fn count_perms(springs: &[char], spaces: &[char]) -> usize {
-  if springs.len() == 0 {
-    // println!("Finished!");
-    spaces.iter().all(|c| *c != '#') as usize
+  let result = if springs.len() == 0 {
+    spaces.iter().all(|c| *c != b'#') as usize
   } else if spaces.len() < springs.len() {
-    // println!("Stuck!");
     0
   } else {
+    // println!("Matching {} and {}", springs[0], spaces[0]);
     match (springs[0], spaces[0]) {
-      (' ', '#') => 0,
-      (' ', _) => count_perms(&springs[1..], &spaces[1..]),
-      ('x', '.') => 0,
-      ('x', _) => count_perms(&springs[1..], &spaces[1..]),
-      ('s', '.') => count_perms(springs, &spaces[1..]),
-      ('s', '#') => count_perms(&springs[1..], &spaces[1..]),
-      ('s', '?') => count_perms(springs, &spaces[1..]) + count_perms(&springs[1..], &spaces[1..]),
+      (b' ', b'#') => 0,
+      (b' ', _) => count_perms(cache, &springs[1..], &spaces[1..]),
+      (b'x', b'.') => 0,
+      (b'x', _) => count_perms(cache, &springs[1..], &spaces[1..]),
+      (b's', b'.') => count_perms(cache, springs, &spaces[1..]),
+      (b's', b'#') => count_perms(cache, &springs[1..], &spaces[1..]),
+      (b's', b'?') => count_perms(cache, springs, &spaces[1..]) + count_perms(cache, &springs[1..], &spaces[1..]),
       _ => unreachable!()
     }
-  }
+  };
+
+  match cache {
+    Some(ca) => {(*ca).insert(key, result);},
+    _ => ()
+  };
+    
+
+  result
 }
 
 #[aoc23::main(12)]
 fn main(input: &str) -> (usize, usize) {
-  // let mut p1 = 0;
-  let mut p2 = 0;
-
-  let spring_data = input.split("\n").map(|row| parse_row(row)).collect_vec();
-  let p1 = spring_data.iter().map(|(springs, spaces)| count_perms(springs, spaces)).collect_vec().iter().sum();
-
-  (p1,p2)
+  input.split("\n").fold((0,0), |acc, row| {
+    let (spaces, raw_counts) = row.split_once(' ').unwrap();
+    let binding = raw_counts.split(',').map(|num| num.parse::<usize>().unwrap())
+    .map(|n| "s".to_owned() + &("x".repeat(n - 1))).collect_vec().join(" ");
+    let springs = binding.as_str();
+    let p1 = count_perms(&mut None, springs.as_bytes(), spaces.as_bytes());
+    let more_springs = (0..5).map(|_| springs).join(" ");
+    let more_spaces = (0..5).map(|_| spaces).join("?");
+    let p2 = count_perms(&mut Some(HashMap::new()), more_springs.as_bytes(), more_spaces.as_bytes());
+    (acc.0 + p1, acc.1 + p2)
+  })
 }
