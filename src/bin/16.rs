31b424111071656438dd4fc1099cdf9629c8a249
Matthijs Wesseling <matthijswesseling@gmail.com>
2023-12-18 17:18:56 +0100

    Replace chars with usize

--- src/bin/16.rs
+++ src/bin/16.rs
@@ -1,87 +1,77 @@
 use hashbrown::{HashMap, HashSet};
 use itertools::Itertools;
+
+enum Direction {
+  N = 0,
+  E = 1,
+  S = 2,
+  W = 3,
+}
+
   
-fn step(direction: char, pos: (usize, usize)) -> (usize, usize) {
+fn step(pos: (usize, usize, usize), direction: usize) -> (usize, usize, usize) {
   match direction {
-    'S' => (pos.0, pos.1 + 1),
-    'N' => (pos.0, pos.1 - 1),
-    'E' => (pos.0 + 1, pos.1),
-    'W' => (pos.0 - 1, pos.1),
+    0 => (pos.0, pos.1 - 1, 0),
+    1 => (pos.0 + 1, pos.1, 1),
+    2 => (pos.0, pos.1 + 1, 2),
+    3 => (pos.0 - 1, pos.1, 3),
     _ => unreachable!()
   }
 }
 
-fn step_back(direction: char, pos: (usize, usize)) -> (usize, usize) {
+fn step_back(pos: (usize, usize, usize), direction: usize) -> (usize, usize, usize) {
   match direction {
-    'S' => (pos.0, pos.1 - 1),
-    'N' => (pos.0, pos.1 + 1),
-    'E' => (pos.0 - 1, pos.1),
-    'W' => (pos.0 + 1, pos.1),
+    0 => (pos.0, pos.1 + 1, 0),
+    1 => (pos.0 - 1, pos.1, 1),
+    2 => (pos.0, pos.1 - 1, 2),
+    3 => (pos.0 + 1, pos.1, 3),
     _ => unreachable!()
   }
 }
 
-fn traverse(grid: &Vec<&[u8]>, direction: char, pos: (usize, usize), size: usize, visited: &mut HashSet<((usize, usize), char)>, paths: &HashMap<((usize, usize), char), ((usize, usize), char, HashSet<((usize,usize), char)>)>) {
+fn traverse(grid: &Vec<&[u8]>, pos: (usize, usize, usize), size: usize, visited: &mut HashSet<(usize, usize, usize)>, paths: &HashMap<(usize, usize, usize), ((usize, usize, usize), HashSet<(usize,usize, usize)>)>) {
   // println!("Heading {} to {} at ({}, {}), seen {}", direction, grid[pos.1 as usize][pos.0 as usize] as char, pos.0, pos.1, visited.len());
-  if visited.contains(&(pos, direction)) || pos.0 >= size || pos.1 >= size {return;}
-  visited.insert((pos, direction));
+  if visited.contains(&pos) || pos.0 >= size || pos.1 >= size {return;}
+  visited.insert(pos);
   let symbol = grid[pos.1 as usize][pos.0 as usize];
-  match (symbol, direction) {
-    (b'.', _)       => traverse(grid, direction, step(direction, pos), size, visited, paths),
-    (b'/', 'N')     => traverse(grid, 'E', step('E', pos), size, visited, paths),
-    (b'/', 'S')     => traverse(grid, 'W', step('W', pos), size, visited, paths),
-    (b'/', 'E')     => traverse(grid, 'N', step('N', pos), size, visited, paths),
-    (b'/', 'W')     => traverse(grid, 'S', step('S', pos), size, visited, paths),
-    (b'\\', 'N')    => traverse(grid, 'W', step('W', pos), size, visited, paths),
-    (b'\\', 'S')    => traverse(grid, 'E', step('E', pos), size, visited, paths),
-    (b'\\', 'E')    => traverse(grid, 'S', step('S', pos), size, visited, paths),
-    (b'\\', 'W')    => traverse(grid, 'N', step('N', pos), size, visited, paths),
-    (b'|', 'N'|'S') => {
-      let (new_pos, new_dir, path) = paths.get(&(pos, direction)).unwrap();
+  match (symbol, pos.2) {
+    (b'/', _)       => traverse(grid, step(pos, [1,0,3,2][pos.2]), size, visited, paths),
+    (b'\\', _)       => traverse(grid, step(pos, [3,2,1,0][pos.2]), size, visited, paths),
+    (b'|', 1|3) => {
+      let (new_pos, path) = paths.get(&(pos.0, pos.1, 0)).unwrap();
       for loc in path {visited.insert(*loc);}
-      // println!("Leaping {} to {} at ({}, {}), seen {}", new_dir, grid[new_pos.1 as usize][new_pos.0 as usize] as char, new_pos.0, new_pos.1, visited.len());
-      traverse(grid, *new_dir, *new_pos, size, visited, paths);
+      traverse(grid, *new_pos, size, visited, paths);
+      let (new_pos, path) = paths.get(&(pos.0, pos.1, 2)).unwrap();
+      for loc in path {visited.insert(*loc);}
+      traverse(grid, *new_pos, size, visited, paths);
     },
-    (b'-', 'E'|'W') => {
-      let (new_pos, new_dir, path) = paths.get(&(pos, direction)).unwrap();
+    (b'-', 0|2) => {
+      let (new_pos, path) = paths.get(&(pos.0, pos.1, 1)).unwrap();
       for loc in path {visited.insert(*loc);}
-      traverse(grid, *new_dir, *new_pos, size, visited, paths);
+      traverse(grid, *new_pos, size, visited, paths);
+      let (new_pos, path) = paths.get(&(pos.0, pos.1, 3)).unwrap();
+      for loc in path {visited.insert(*loc);}
+      traverse(grid, *new_pos, size, visited, paths);
     },
-    (b'|', 'E'|'W') => {
-      let (new_pos, new_dir, path) = paths.get(&(pos, 'N')).unwrap();
+    (b'|'|b'-', _) => {
+      let (new_pos, path) = paths.get(&pos).unwrap();
       for loc in path {visited.insert(*loc);}
-      traverse(grid, *new_dir, *new_pos, size, visited, paths);
-      let (new_pos, new_dir, path) = paths.get(&(pos, 'S')).unwrap();
-      for loc in path {visited.insert(*loc);}
-      traverse(grid, *new_dir, *new_pos, size, visited, paths);
+      traverse(grid, *new_pos, size, visited, paths);
     },
-    (b'-', 'N'|'S') => {
-      let (new_pos, new_dir, path) = paths.get(&(pos, 'E')).unwrap();
-      for loc in path {visited.insert(*loc);}
-      traverse(grid, *new_dir, *new_pos, size, visited, paths);
-      let (new_pos, new_dir, path) = paths.get(&(pos, 'W')).unwrap();
-      for loc in path {visited.insert(*loc);}
-      traverse(grid, *new_dir, *new_pos, size, visited, paths);
-    },
+    (b'.', _)       => traverse(grid, step(pos, [0,1,2,3][pos.2]), size, visited, paths),
     _ => unreachable!()
   };
 }
 
-fn find_next_split(grid: &Vec<&[u8]>, direction: char, pos: (usize, usize), size: usize, visited: &mut HashSet<((usize, usize), char)>) -> ((usize, usize), char) {
-  if pos.0 >= size || pos.1 >= size {return (step_back(direction, pos), direction);}
+fn find_next_split(grid: &Vec<&[u8]>, pos: (usize, usize, usize), size: usize, visited: &mut HashSet<(usize, usize, usize)>) -> (usize, usize, usize) {
+  if pos.0 >= size || pos.1 >= size {return step_back(pos, pos.2);}
   let symbol = grid[pos.1 as usize][pos.0 as usize];
-  if symbol == b'-' || symbol == b'|' {return (pos, direction)};
-  visited.insert((pos, direction));
-  match (symbol, direction) {
-    (b'.', _)       => find_next_split(grid, direction, step(direction, pos), size, visited),
-    (b'/', 'N')     => find_next_split(grid, 'E', step('E', pos), size, visited),
-    (b'/', 'S')     => find_next_split(grid, 'W', step('W', pos), size, visited),
-    (b'/', 'E')     => find_next_split(grid, 'N', step('N', pos), size, visited),
-    (b'/', 'W')     => find_next_split(grid, 'S', step('S', pos), size, visited),
-    (b'\\', 'N')    => find_next_split(grid, 'W', step('W', pos), size, visited),
-    (b'\\', 'S')    => find_next_split(grid, 'E', step('E', pos), size, visited),
-    (b'\\', 'E')    => find_next_split(grid, 'S', step('S', pos), size, visited),
-    (b'\\', 'W')    => find_next_split(grid, 'N', step('N', pos), size, visited),
+  if symbol == b'-' || symbol == b'|' {return pos};
+  visited.insert(pos);
+  match (symbol, pos.2) {
+    (b'.', _)       => find_next_split(grid, step(pos, [0,1,2,3][pos.2]), size, visited),
+    (b'/', _)       => find_next_split(grid, step(pos, [1,0,3,2][pos.2]), size, visited),
+    (b'\\', _)      => find_next_split(grid, step(pos, [3,2,1,0][pos.2]), size, visited),
     _ => unreachable!()
   }
 }
@@ -92,38 +82,38 @@ fn main(input: &str) -> (usize, usize) {
   let size = grid.len() as usize;
 
   let mut visited = HashSet::new();
-  let mut paths = HashMap::<((usize, usize), char), ((usize, usize), char, HashSet<((usize,usize), char)>)>::new();
+  let mut paths = HashMap::<(usize, usize, usize), ((usize, usize, usize), HashSet<(usize,usize,usize)>)>::new();
   
   for j in 0..size {
     for i in 0..size {
       let symbol = grid[j as usize][i as usize];
       match symbol {
-        b'|' => for direction in vec!['N','S'] {
-          let mut visited = HashSet::<((usize,usize), char)>::new();
-          let (new_pos, new_dir) = find_next_split(&grid, direction, step(direction, (i,j)), size, &mut visited);
+        b'|' => for direction in vec![0,2] {
+          let mut visited = HashSet::<(usize,usize,usize)>::new();
+          let new_pos = find_next_split(&grid, step((i,j,direction), direction), size, &mut visited);
           // println!("{}, {} -> {}", i, j, visited.len());
-          paths.insert(((i,j), direction), (new_pos, new_dir, visited.clone()));
+          paths.insert((i,j,direction), (new_pos, visited.clone()));
         },
-        b'-' => for direction in vec!['E','W'] {
-          let mut visited = HashSet::<((usize,usize), char)>::new();
-          let (new_pos, new_dir) = find_next_split(&grid, direction, step(direction, (i,j)), size, &mut visited);
+        b'-' => for direction in vec![1,3] {
+          let mut visited = HashSet::<(usize,usize,usize)>::new();
+          let new_pos = find_next_split(&grid, step((i,j,direction), direction), size, &mut visited);
           // println!("{}, {} -> {}", i, j, visited.len());
-          paths.insert(((i,j), direction), (new_pos, new_dir, visited.clone()));
+          paths.insert((i,j,direction), (new_pos, visited.clone()));
         },
         _ => ()
       }
     }
   }
 
-  traverse(&grid, 'E', (0, 0), size, &mut visited, &paths);
-  let p1 = visited.into_iter().map(|(pos, _)| pos).collect::<HashSet<_>>().len();
-  let p2 = (0..size).flat_map(|i| [((size - 1, i), 'W'), ((0, i), 'E')])
-    .chain((0..size).flat_map(|i| [((i, size - 1), 'N'), ((i, 0), 'S')]))
-    .map(|(pos, direction)| {
+  traverse(&grid, (0, 0, 1), size, &mut visited, &paths);
+  let p1 = visited.into_iter().map(|(x, y, _)| (x, y)).collect::<HashSet<_>>().len();
+  let p2 = (0..size).flat_map(|i| [(size - 1, i, 3), (0, i, 1)])
+    .chain((0..size).flat_map(|i| [(i, size - 1, 0), (i, 0, 2)]))
+    .map(|pos| {
       let mut visited = HashSet::new();
-      traverse(&grid, direction, pos, size, &mut visited, &paths);
-      let result = visited.into_iter().map(|(pos, _)| pos).collect::<HashSet<_>>().len();
-      println!("Start ({}, {}) dir {} len {}", pos.0, pos.1, direction, result);
+      traverse(&grid, pos, size, &mut visited, &paths);
+      let result = visited.into_iter().map(|(x, y, _)| (x, y)).collect::<HashSet<_>>().len();
+      // println!("Start ({}, {}) dir {} len {}", pos.0, pos.1, pos.2, result);
       result
     })
     .max().unwrap();
