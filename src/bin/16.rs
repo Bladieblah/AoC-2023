ce53dd0bfe328c235f5dc1691c3d1ac8dc11ab33
Matthijs Wesseling <matthijswesseling@gmail.com>
2023-12-19 11:15:55 +0100

    Massive speedup but buggy

--- src/bin/16.rs
+++ src/bin/16.rs
@@ -20,8 +20,8 @@ fn step(pos: (usize, usize, usize), direction: usize) -> (usize, usize, usize) {
 }
 
 fn traverse(grid: &Vec<&[u8]>, pos: (usize, usize, usize), size: usize, visited: &mut Vec<Vec<Vec<bool>>>) {
-  if pos.0 >= size || pos.1 >= size || visited[pos.0][pos.1][pos.2] {return;}
-  visited[pos.0][pos.1][pos.2] = true;
+  if pos.0 >= size || pos.1 >= size || visited[pos.1][pos.0][pos.2] {return}
+  visited[pos.1][pos.0][pos.2] = true;
   let symbol = grid[pos.1 as usize][pos.0 as usize];
   match (symbol, pos.2) {
     (b'/', _)  => traverse(grid, step(pos, [1,0,3,2][pos.2]), size, visited),
@@ -34,41 +34,28 @@ fn traverse(grid: &Vec<&[u8]>, pos: (usize, usize, usize), size: usize, visited:
       traverse(grid, step(pos, 1), size, visited);
       traverse(grid, step(pos, 3), size, visited);
     },
-    (_, _)      => traverse(grid, step(pos, pos.2), size, visited),
+    (_, _) => traverse(grid, step(pos, pos.2), size, visited),
   };
 }
 
-fn traverse2(grid: &Vec<&[u8]>, pos: (usize, usize, usize), size: usize, visited: &mut HashSet<(usize, usize, usize)>, paths: &HashMap<(usize, usize, usize), ((usize, usize, usize), HashSet<(usize,usize, usize)>)>, full_loop: &Vec<Vec<Vec<bool>>>) {
-  // println!("Heading {} to {} at ({}, {}), seen {}", direction, grid[pos.1 as usize][pos.0 as usize] as char, pos.0, pos.1, visited.len());
-  if pos.0 >= size || pos.1 >= size || visited.contains(&pos) {return;}
+fn traverse2(grid: &Vec<&[u8]>, pos: (usize, usize, usize), size: usize, visited: &mut HashSet<(usize, usize, usize)>, main_loop: &Vec<Vec<Vec<bool>>>) -> bool {
+  if pos.0 >= size || pos.1 >= size {return false}
+  if main_loop[pos.1][pos.0][pos.2] {return true}
+  if visited.contains(&pos) {return false}
   visited.insert(pos);
   let symbol = grid[pos.1 as usize][pos.0 as usize];
-  match (symbol, pos.2) {
-    (b'/', _) => traverse2(grid, step(pos, [1,0,3,2][pos.2]), size, visited, paths, full_loop),
-    (b'\\', _) => traverse2(grid, step(pos, [3,2,1,0][pos.2]), size, visited, paths, full_loop),
-    (b'|', 1|3) => {
-      let (new_pos, path) = paths.get(&(pos.0, pos.1, 0)).unwrap();
-      for pos in path {visited.insert(*pos);}
-      traverse2(grid, *new_pos, size, visited, paths, full_loop);
-      let (new_pos, path) = paths.get(&(pos.0, pos.1, 2)).unwrap();
-      for pos in path {visited.insert(*pos);}
-      traverse2(grid, *new_pos, size, visited, paths, full_loop);
-    },
+  return match (symbol, pos.2) {
+    (b'/', _)  => traverse2(grid, step(pos, [1,0,3,2][pos.2]), size, visited, main_loop),
+    (b'\\', _) => traverse2(grid, step(pos, [3,2,1,0][pos.2]), size, visited, main_loop),
+    (b'|', 1|3) => 
+      traverse2(grid, step(pos, 0), size, visited, main_loop) ||
+      traverse2(grid, step(pos, 2), size, visited, main_loop)
+    ,
     (b'-', 0|2) => {
-      let (new_pos, path) = paths.get(&(pos.0, pos.1, 1)).unwrap();
-      for pos in path {visited.insert(*pos);}
-      traverse2(grid, *new_pos, size, visited, paths, full_loop);
-      let (new_pos, path) = paths.get(&(pos.0, pos.1, 3)).unwrap();
-      for pos in path {visited.insert(*pos);}
-      traverse2(grid, *new_pos, size, visited, paths, full_loop);
+      traverse2(grid, step(pos, 1), size, visited, main_loop) ||
+      traverse2(grid, step(pos, 3), size, visited, main_loop)
     },
-    (b'|'|b'-', _) => {
-      let (new_pos, path) = paths.get(&pos).unwrap();
-      for pos in path {visited.insert(*pos);}
-      traverse2(grid, *new_pos, size, visited, paths, full_loop);
-    },
-    (b'.', _) => traverse2(grid, step(pos, [0,1,2,3][pos.2]), size, visited, paths, full_loop),
-    _ => unreachable!()
+    (_, _) => traverse2(grid, step(pos, pos.2), size, visited, main_loop),
   };
 }
 
@@ -117,29 +104,47 @@ fn main(input: &str) -> (usize, usize) {
 
   // println!("I made {} paths", paths.len());
   let mut full_loop = vec![vec![vec![false; 4]; size]; size];
+  let mut loop_length = 0;
   if let Some(seed_loop) = find_loop(&grid) {
     traverse(&grid, *seed_loop.first().unwrap(), size, &mut full_loop);
-    println!("Woah cluster of length {}!", full_loop.iter().flat_map(|row| row).fold(0, |acc, cell| acc + cell.iter().any(|d| *d) as usize));
+    loop_length = full_loop.iter().flat_map(|row| row).fold(0, |acc, cell| acc + cell.iter().any(|d| *d) as usize);
+    println!("Woah cluster of length {}!", loop_length);
   }
 
-  // let mut visited = HashSet::new();
-  // traverse2(&grid, (0, 0, 1), size, &mut visited, &paths, &full_loop);
-  // let p1 = visited.iter().flat_map(|row| row).fold(0, |acc, cell| acc + cell.iter().any(|d| *d) as usize);
+  // for row in &full_loop {
+  //   for cell in row {
+  //     let s = if cell.iter().any(|&c| c) {'#'} else {'.'};
+  //     print!("{}", s);
+  //   }
+  //   println!("");
+  // }
 
-  let mut p1 = 0;
+  let mut visited = HashSet::new();
+  let hit = traverse2(&grid, (0, 0, 1), size, &mut visited, &full_loop);
+  let p1 = if !hit {
+    visited.iter().map(|(x,y,_)| (x,y)).collect::<HashSet<_>>().len()
+  } else {
+    visited.iter().map(|(x,y,_)| (*x,*y)).fold(loop_length, |acc, (x, y)| acc + full_loop[y][x].iter().all(|&c| !c) as usize)
+  };
+
+  // let mut p1 = 0;
   let mut p2 = 0;
-  // let p2 = (0..size).flat_map(|i| [(size - 1, i, 3), (0, i, 1)])
-  //   .chain((0..size).flat_map(|i| [(i, size - 1, 0), (i, 0, 2)]))
-  //   .map(|pos| {
-  //     // let now = ::std::time::Instant::now();
-  //     let mut visited = vec![vec![vec![false; 4]; size]; size];
-  //     traverse(&grid, pos, size, &mut visited, &paths);
-  //     let result = visited.iter().flat_map(|row| row).fold(0, |acc, cell| acc + cell.iter().any(|d| *d) as usize);
-  //     // let elapsed = now.elapsed();
-  //     // println!("Start ({}, {}) dir {} len {}, took {}μs", pos.0, pos.1, pos.2, result, elapsed.as_micros());
-  //     result
-  //   })
-  //   .max().unwrap();
+  let p2 = (0..size).flat_map(|i| [(size - 1, i, 3), (0, i, 1)])
+    .chain((0..size).flat_map(|i| [(i, size - 1, 0), (i, 0, 2)]))
+    .map(|pos| {
+      let now = ::std::time::Instant::now();
+      let mut visited = HashSet::new();
+      let hit = traverse2(&grid, pos, size, &mut visited, &full_loop);
+      let result = if !hit {
+        visited.iter().map(|(x,y,_)| (x,y)).collect::<HashSet<_>>().len()
+      } else {
+        visited.iter().map(|(x,y,_)| (*x,*y)).fold(loop_length, |acc, (x, y)| acc + full_loop[y][x].iter().all(|&c| !c) as usize)
+      };
+      let elapsed = now.elapsed();
+      println!("Start ({}, {}) dir {} len {}, took {}μs", pos.0, pos.1, pos.2, result, elapsed.as_micros());
+      result
+    })
+    .max().unwrap();
 
   (p1,p2)
 }
