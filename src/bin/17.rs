24743c9f9fe07ab0f22f4c7f7f6de08d728455f6
Matthijs Wesseling <matthijswesseling@gmail.com>
2023-12-19 15:02:22 +0100

    Cleanup

--- src/bin/17.rs
+++ src/bin/17.rs
@@ -5,8 +5,6 @@ use itertools::Itertools;
 fn shortest_path(city: &Vec<Vec<i32>>, size: usize, mi: usize, ma: usize) -> i32 {
   let mut visited = vec![vec![vec![std::i32::MIN; 2]; size]; size];
   let dirs: &Vec<(usize, usize)> = &vec![(0, 1), (1, 0)];
-  
-  // (score, (x,y,d))
   let mut paths = BinaryHeap::<(i32,(usize,usize,usize))>::from_iter([(0, (0,0,2))]);
 
   while let Some((score, (x, y, dir))) = paths.pop() {
@@ -60,9 +58,6 @@ fn shortest_path(city: &Vec<Vec<i32>>, size: usize, mi: usize, ma: usize) -> i32
 
 #[aoc23::main(17)]
 fn main(input: &str) -> (usize, usize) {
-  let mut p1 = 0;
-  let mut p2 = 0;
-
   let city = input.split_whitespace().map(|row| row.as_bytes().iter().map(|x| (x - b'0') as i32).collect_vec()).collect_vec();
   let size = city.len();
 
